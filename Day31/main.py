699693bca64c20102c4ecedc36720dd4fcaf9c32
Seungmin Ahn <smahn9123@gmail.com>
2024-11-29 20:56:39 +0900

    feat: Day31 step1 complete
    Signed-off-by: Seungmin Ahn <smahn9123@gmail.com>

--- Day31/main.py
+++ Day31/main.py
@@ -1,2 +1,26 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+title_text = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", font=("Ariel", 50, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_img = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+
+right_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_img, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+
+
+window.mainloop()

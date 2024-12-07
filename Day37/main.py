f5fafee330aac16dc90c571f0bf6eb088406f521
SeungminAhn <smahn9123@gmail.com>
2024-12-07 20:51:17 +0900

    feat: add PUT and DELETE for API request in Day37
    Signed-off-by: SeungminAhn <smahn9123@gmail.com>

--- Day37/main.py
+++ Day37/main.py
@@ -43,5 +43,19 @@ pixel_data = {
     "quantity": "9.74",
 }
 
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
 print(response.text)

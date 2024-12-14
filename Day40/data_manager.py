62694e83fa26c434c6c4bb23726e41e5392839a3
SeungminAhn <smahn9123@gmail.com>
2024-12-14 14:59:24 +0900

    feat: get users email list in Day40
    Signed-off-by: SeungminAhn <smahn9123@gmail.com>

--- Day40/data_manager.py
+++ Day40/data_manager.py
@@ -7,14 +7,15 @@ load_dotenv()
 
 class DataManager:
     def __init__(self):
-        self.endpoint = os.getenv("SHEETY_ENDPOINT")
+        self.endpoint_prices = os.getenv("SHEETY_ENDPOINT_PRICES")
+        self.endpoint_users = os.getenv("SHEETY_ENDPOINT_USERS")
         self.header = {
             "Authorization": os.getenv("SHEETY_TOKEN")
         }
         self.auth = HTTPBasicAuth(os.getenv("SHEETY_USERNAME"), os.getenv("SHEETY_PASSWORD"))
 
     def get_prices(self):
-        response = requests.get(url=self.endpoint, headers=self.header)
+        response = requests.get(url=self.endpoint_prices, headers=self.header)
         return response.json()["prices"]
 
     def update_iata_code(self, iata_code, id):
@@ -23,8 +24,13 @@ class DataManager:
                 "iataCode": iata_code
             }
         }
-        response = requests.put(url=f"{self.endpoint}/{id}", json=sheety_iata_code_params,
+        response = requests.put(url=f"{self.endpoint_prices}/{id}", json=sheety_iata_code_params,
                                 headers=self.header
                                 # auth=self.auth
                                 )
         return response.json()
+
+    def get_customer_emails(self):
+        response = requests.get(url=self.endpoint_users, headers=self.header)
+        users_data = response.json()["users"]
+        return [user_data["whatIsYourEmail?"] for user_data in users_data]
